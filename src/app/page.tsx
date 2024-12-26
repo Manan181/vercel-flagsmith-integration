a6fe8938b3a17de67e22e6026092139c5a8173da
Manan Sonani <manan.s@mail.biztechcs.com>
2024-12-26 14:55:39 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { Jost } from 'next/font/google';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useFlagsmith } from 'flagsmith/react';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const jost = Jost({ subsets: ['latin'] });
+
+export type User = {
+	name: string;
+	position: string;
+};
+
+export default function Page() {
+  const flagsmith = useFlagsmith();
+  const [user, toggleUser] = useState<User>({
+		name: '',
+		position: 'freelancer',
+  });
+  
+  const router = useRouter();
+  
+  const handleUserName = (e: ChangeEvent<HTMLInputElement>) => {
+		toggleUser({
+			...user,
+			name: e.target.value,
+		});
+  };
+  
+  const handleUserPosition = (e: ChangeEvent<HTMLSelectElement>) => {
+		toggleUser({
+			...user,
+			position: e.target.value,
+		});
+  };
+  
+  const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		await flagsmith.setContext({ identity: { traits: { 'profession': user.position } } });
+		router.push('/about?name=' + user.name + '&position=' + user.position);
+  };
+  
+	return (
+		<>
+			<main className={`main-login ${jost.className}`}>
+				<div className="login-box">
+					<h1>Login</h1>
+					<form className="login-form" onSubmit={handleOnSubmit}>
+						<input type="text" placeholder="Username" className="login-input" onChange={handleUserName} />
+						<select className="login-menu" onChange={handleUserPosition}>
+							<option className="login-menu-item" defaultValue="freelancer">
+								Freelancer
+							</option>
+							<option className="login-menu-item" value="front-end-developer">
+								Front-end developer
+							</option>
+							<option className="login-menu-item" value="designer">
+								Designer
+							</option>
+						</select>
+						<button className="login-form-btn" type="submit">
+							Login
+						</button>
+					</form>
+				</div>
+			</main>
+		</>
+	);
 }
